c65a18f86ba3d84abd80b5a896b29fb5c1dbf63f
JIM <icekjm1029@gmail.com>
2023-08-05 19:02:50 +0900

    fix: lib 컨트롤러에 @CrossOrigin(origins = "http://localhost:3000")  어노테이션 추가

--- src/main/java/com/ant/ttf/domain/library/controller/LibController.java
+++ src/main/java/com/ant/ttf/domain/library/controller/LibController.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +25,7 @@ import com.ant.ttf.global.ResponseStatus;
 
 import lombok.extern.java.Log;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/library")
 @Log
